cefcf3cbd1f3f97cf7709a6c9c353ee8350b9354
Pontus Malm <malm@chalmers.it>
2016-06-30 00:40:36 +0200

    Thumbnails

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,5 @@
 class Video < ActiveRecord::Base
-  has_attached_file :video
+  has_attached_file :video, styles: {thumb: ["150x200#", :png]}
   validates_attachment_content_type :video, content_type: /\Avideo/
   validates_attachment_presence :video
   validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
